606f86d004012bd2519ed1bf7dfefe149ac37ac4
dpi <pro@danielph.in>
2021-07-23 15:40:37 +0800

    ✨ Added clone project merge request command

---
+++ src/Commands/Options/CloneProjectMergeRequestOptions.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dogit\Commands\Options;
+
+use Symfony\Component\Console\Input\InputInterface;
+
+final class CloneProjectMergeRequestOptions
+{
+    public const ARGUMENT_DIRECTORY = 'directory';
+    public const ARGUMENT_PROJECT = 'project';
+    public const OPTION_ALL = 'all';
+    public const OPTION_HTTP = 'http';
+    public const OPTION_ONLY_CLOSED = 'include-closed';
+    public const OPTION_ONLY_MERGED = 'include-merged';
+    public const OPTION_NO_CACHE = 'no-cache';
+
+    public string $directory;
+    public string $project;
+    public bool $includeAll;
+    public bool $isHttp;
+    public bool $onlyClosed;
+    public bool $onlyMerged;
+    public bool $noHttpCache;
+
+    public static function fromInput(InputInterface $input): static
+    {
+        $instance = new static();
+
+        // @phpstan-ignore-next-line
+        $instance->directory = $input->getArgument(static::ARGUMENT_DIRECTORY);
+        // @phpstan-ignore-next-line
+        $instance->project = $input->getArgument(static::ARGUMENT_PROJECT);
+        // @phpstan-ignore-next-line
+        $instance->isHttp = $input->getOption(static::OPTION_HTTP);
+        $instance->includeAll = (bool) $input->getOption(static::OPTION_ALL);
+        $instance->onlyClosed = (bool) $input->getOption(static::OPTION_ONLY_CLOSED);
+        $instance->onlyMerged = (bool) $input->getOption(static::OPTION_ONLY_MERGED);
+
+        return $instance;
+    }
+}
